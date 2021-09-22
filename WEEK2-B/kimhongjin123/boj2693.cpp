b3fa5dc14d2c5510aa70b5e0dd4c522008d4bd2d
kimhongjin123 <ghdwls0604@naver.com>
2021-09-23 01:05:14 +0900

    Create boj2693.cpp

---
+++ WEEK2-B/kimhongjin123/boj2693.cpp
@@ -0,0 +1,46 @@
+﻿#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	int** array;
+	int length;
+	cin >> length;
+	array = new int* [length];
+
+	for (int i = 0; i < length; ++i)
+	{
+		array[i] = new int[10];
+	}
+	for (int i = 0; i < length; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			int num = 0;
+			cin >> num;
+			array[i][j] = num;
+
+		}
+		sort(array[i], array[i] + 10);
+
+	}
+	for (int i = 0; i < length; i++)
+	{
+
+		cout << array[i][7] << '\n';
+
+	}
+
+
+
+
+
+
+
+
+
+
+	return 0;
+
+}
