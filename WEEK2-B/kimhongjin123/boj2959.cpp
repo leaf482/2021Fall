49be694c4230407e236e0f98d8ccb59e5226342f
kimhongjin123 <ghdwls0604@naver.com>
2021-09-23 01:05:22 +0900

    Create boj2959.cpp

---
+++ WEEK2-B/kimhongjin123/boj2959.cpp
@@ -0,0 +1,25 @@
+﻿#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	vector<int>v;
+
+
+	for (int i = 0; i < 4; i++)
+	{
+		int n;
+		cin >> n;
+		v.push_back(n);
+
+	}
+	sort(v.begin(), v.end());
+
+	int answer = v.front() * v[2];
+	cout << answer;
+
+
+	return 0;
+
+}
