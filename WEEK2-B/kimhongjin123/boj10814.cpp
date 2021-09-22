8dec5ad0cd83d0bcdfecbdf619aa054a193c85b8
kimhongjin123 <ghdwls0604@naver.com>
2021-09-23 01:05:05 +0900

    Create boj10814.cpp

---
+++ WEEK2-B/kimhongjin123/boj10814.cpp
@@ -0,0 +1,40 @@
+﻿
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+bool cmp(pair<int, string> a, pair<int, string> b)
+{
+		return a.first < b.first;
+}
+
+int main()
+
+{
+	vector<pair<int, string>>v;
+	string name;
+	int old;
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> old>> name;
+		
+		v.push_back(make_pair(old, name));
+
+	}
+	
+	stable_sort(v.begin(), v.end(),cmp);
+
+	for (int i = 0; i < v.size(); i++) {
+		cout << v[i].first << " " << v[i].second << '\n';
+	}
+
+
+}
