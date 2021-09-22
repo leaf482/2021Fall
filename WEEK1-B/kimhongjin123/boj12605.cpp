9d473b8780b85db146de9f6ed5ae8651ba427ef0
kimhongjin123 <ghdwls0604@naver.com>
2021-09-23 01:02:56 +0900

    Create boj12605.cpp

---
+++ WEEK1-B/kimhongjin123/boj12605.cpp
@@ -0,0 +1,55 @@
+﻿#include<sstream>
+#include <vector>
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	int count = 1;
+	int j;
+	
+
+		string s;
+		
+		cin >> j;
+		cin.ignore( ); // 입력 버퍼 지우기
+		for(int num=0;num <j;num++)
+		{
+			
+			
+			getline(cin, s); //스페이스바 엔터 취급 x
+			
+			istringstream s1(s);
+			string s2;
+			vector<string>v;
+			
+			while (getline(s1, s2, ' ')) //스페이스바를 분기점을 백터에 나눠줌
+			{
+				v.push_back(s2);
+			}
+			
+			
+				cout << "Case #" << count << ": "; //아니 띄어쓰기 하나는 오바잖아 ......
+				
+				for (int i = v.size() - 1; i >= 0; i--) {
+					
+					if (i == 0)
+					{
+						cout << v[i];
+					}
+					else {
+						cout << v[i] << " "; //여기도 마찬가지.......
+					}
+				}
+				cout << endl;
+				
+				
+				
+			
+			v.clear();
+			count++;
+		}
+	
+		return 0;
+}
+
