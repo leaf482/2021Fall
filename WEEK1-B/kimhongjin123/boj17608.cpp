a083c0b8f5748006f15855f11d35927cbbd325f5
kimhongjin123 <ghdwls0604@naver.com>
2021-09-23 01:03:01 +0900

    Create boj17608.cpp

---
+++ WEEK1-B/kimhongjin123/boj17608.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int j;
+    int count = 1;
+    vector <int> v;
+    cin >> j;
+
+    for (int i = 0; i < j; i++)
+    {
+        int num = 0;
+        cin >> num;
+        v.push_back(num);
+    }
+    
+    for (int i = j; i>= 2; i--)
+    {
+        if (v[i - 1] < v[i - 2])
+        {
+            count++;
+        }
+        else
+        {
+            v[i - 2] = v[i - 1];
+            
+        }
+        
+    }
+    cout << count;
+}
+
