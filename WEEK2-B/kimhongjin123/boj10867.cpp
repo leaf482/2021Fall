414ed6b6085351dbb93a832302835439bab9b00f
kimhongjin123 <ghdwls0604@naver.com>
2021-09-23 01:05:10 +0900

    Create boj10867.cpp

---
+++ WEEK2-B/kimhongjin123/boj10867.cpp
@@ -0,0 +1,33 @@
+﻿#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main()
+{
+	vector<int>v;
+	int n;
+	cin >> n;
+	
+	for (int i = 0; i < n; i++)
+	{
+		int num;
+		cin >> num;
+		auto s = find(v.begin(), v.end(), num);
+	
+		if (s==v.end()) 
+		{
+			v.push_back(num);
+		}	
+
+	}
+	sort(v.begin(), v.end());
+
+	for(int i=0;i<v.size();i++)
+	{
+		cout << v[i]<<' ';
+		
+	}
+
+}
+
+
