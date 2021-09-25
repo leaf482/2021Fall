83f2730406955efb202bf538552861425a0a8e37
kimhongjin123 <ghdwls0604@naver.com>
2021-09-26 08:03:01 +0900

    Create boj5086.cpp

---
+++ WEEK3-B/kimhongjin123/boj5086.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <list>
+using namespace std;
+
+int main()
+{
+	while(true)
+	{	int a;
+		int b;
+		cin >> a >> b;
+		if (a != b) 
+		{
+			if (b % a == 0)
+			{
+				cout << "factor" << '\n';
+			}
+			else if (a % b == 0)
+			{
+				cout << "multiple" << '\n';
+			}
+			else
+			{
+				cout << "neither" << '\n';
+			}
+			}
+		else
+		{
+			break;
+		}
+
+		}
+	
+	}
+   
+
