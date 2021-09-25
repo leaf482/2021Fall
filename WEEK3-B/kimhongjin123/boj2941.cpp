7aaa1ad6a18c34a09724736b98f963a9070988ac
kimhongjin123 <ghdwls0604@naver.com>
2021-09-26 08:02:56 +0900

    Create boj2941.cpp

---
+++ WEEK3-B/kimhongjin123/boj2941.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include<string>
+#include <list>
+using namespace std;
+int main()
+{
+	
+	string a;
+	cin >> a;
+	int size = a.size();
+	list<string>s;
+	s.push_back("c=");
+	s.push_back("c-");
+	s.push_back("dz=");
+	s.push_back("d-");
+	s.push_back("lj");
+	s.push_back("nj");
+	s.push_back("s=");
+	s.push_back("z=");
+	
+	list<string>::iterator iter = s.begin();
+	
+	for (iter = s.begin(); iter != s.end(); iter++)
+	{
+		string k = *iter;	
+		while (1)
+		{
+			if (a.find(*iter) != string::npos) {
+				int index = a.find(*iter);
+				a.erase(index,k.size());
+				a.insert(index,k.size(),' ');			
+				size -= k.size() - 1;
+			}
+			else
+			{		
+				break;
+			}
+		}	
+	}
+	cout << size << endl;
+   
+}
+
