c985456400a8b30f9bd4904df31e40f6a62ca8f1
kimhongjin123 <ghdwls0604@naver.com>
2021-10-03 05:31:57 +0900

    Create boj2630.cpp

---
+++ WEEK4-B/kimhongjin123/boj2630.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+int box[129][129], b = 0, w = 0;
+
+void cut(int x, int y, int size) {
+    int stack = 0;
+    for (int i = x; i < x + size; i++) {
+        for (int j = y; j < y + size; j++) {
+            if (box[i][j])
+            {
+                stack++;
+            }
+        }
+    }
+
+    if (!stack)
+    {
+        w++;
+    }
+    else if (stack == size * size)
+    {
+        b++;
+    }
+    else {
+        cut(x, y, size / 2);
+        cut(x + size / 2, y, size / 2);
+        cut(x, y + size / 2, size / 2);
+        cut(x + size / 2, y + size / 2, size / 2);
+    }
+    return;
+}
+
+int main(void) {
+    cin.tie(0); cin.sync_with_stdio(0);
+    int num; cin >> num;
+
+    for (int i = 1; i <= num; i++) {
+        for (int j = 1; j <= num; j++)
+            cin >> box[i][j];
+    }
+
+    cut(1, 1, num);
+    cout << w << "\n"; cout << b << "\n";
+    return 0;
+}
