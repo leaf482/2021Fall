64f0ab43fdbd0dac4d245598aab7b8aded98e114
kimhongjin123 <ghdwls0604@naver.com>
2021-10-03 05:31:18 +0900

    Create boj10829.cpp

---
+++ WEEK4-B/kimhongjin123/boj10829.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+void num(long  n)
+{
+	if (n == 1)
+	{
+		cout << n;
+		return;
+	}
+	else if (n == 0)
+	{
+	
+		return;
+	}
+	
+	num(n / 2);
+	cout << n % 2;
+}
+int main()
+{
+	long  n;
+	cin >> n;
+	num(n);
+	
+	return 0;
+}
+
